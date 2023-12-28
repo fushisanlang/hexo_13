5bef9ee2be1f29d7711a4f1c02343d0b8062e30d
fushisanlang <313346216@qq.com>
2023-12-28 03:25:59 +0000

    add
    Signed-off-by: fushisanlang <313346216@qq.com>

---
+++ source/_posts/note/go/demo/chineseprint.go
@@ -0,0 +1,37 @@
+---
+title: 中文输出
+date: 2023-12-28
+
+tags:
+  - go
+  - demo
+categories:
+  - demo
+---
+windows下，cmd对中文指出不好，通过fmt包输出的中文会乱码。   
+可以使用以下demo，对cmd的语言预配置。   
+但是目前依旧存在跨平台的问题，windows平台才能使用。所以实际使用中可能需要不同的入口文件对应不同的操作系统。   
+```golang
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"syscall"
+
+)
+
+func main() {
+	// 判断操作系统类型
+	if runtime.GOOS == "windows" {
+		// Windows API函数声明
+		kernel32           := syscall.NewLazyDLL("kernel32.dll")
+		setConsoleOutputCP := kernel32.NewProc("SetConsoleOutputCP")
+		setConsoleOutputCP.Call(uintptr(936))
+	}
+
+	// 输出中文测试
+	fmt.Println("中文输出测试")
+}
+
+```
